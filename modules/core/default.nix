eea53fa5added6c6c1d67809601ec66539608659
Samiul Basir Fahim <samiulbasirfahim360@gmail.com>
2023-06-03 01:25:07 +0600

    updated everything

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -14,11 +14,18 @@ in {
   nixos = nixpkgs.lib.nixosSystem {
     specialArgs = {inherit self inputs;};
     modules =
-    [(import ./bootloader.nix)]
-    ++ [(import ./wayland.nix)]
-    ++ [(import ./user.nix)]
-    ++ [(import ./security.nix)]
-    ++ [(import ./programs.nix)]
-    ++ [(import ./system.nix)];
+      [(import ./bootloader.nix)]
+      ++ [(import ./hardware.nix)]
+      ++ [(import ./network.nix)]
+      ++ [(import ./pipewire.nix)]
+      ++ [(import ./program.nix)]
+      ++ [(import ./../home/package/python.nix)]
+      ++ [(import ./security.nix)]
+      ++ [(import ./services.nix)]
+      ++ [(import ./system.nix)]
+      ++ [(import ./user.nix)]
+      ++ [(import ./wayland.nix)]
+      ++ [(import ./cloudflare-warp.nix)]
+      ++ [(import ./../../hosts/nixos/hardware-configuration.nix)];
   };
 }
