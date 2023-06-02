947d4dc5ffedbfb791656dcd2a89e0fe036f8f8b
Samiul Basir Fahim <samiulbasirfahim360@gmail.com>
2023-06-03 01:20:56 +0600

    package copied from prev config file

---
+++ modules/home/package/default.nix
@@ -0,0 +1,79 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [(import ./firefox)];
+  #    [ (import ./python.nix) ];
+
+  home.packages = let
+    php = pkgs.php.buildEnv {extraConfig = "memory_limit = 2G";};
+  in (with pkgs;
+    [
+      # programming
+      python39
+      nodejs
+      nodePackages.nodemon
+      nodePackages.pm2
+      yarn
+      gcc
+      nix-prefetch-github
+      vscode
+      typescript
+      rnix-lsp
+      ripgrep
+      rustup
+      neovide
+      gnumake
+      php
+
+      # others
+      aichat
+      dmenu
+      libnotify
+      dmidecode
+      ranger
+      ueberzug
+      pamixer
+      xfce.thunar
+      pavucontrol
+      wget
+      unzip
+      gparted
+      mpv
+      tty-clock
+      playerctl
+      qalculate-gtk
+      bleachbit
+      imv
+      cmatrix
+      cava
+      git
+      htop
+      xdg-utils
+      fzf
+      ffmpeg
+      mpd
+      ncmpcpp
+      bore-cli
+      webcord
+      google-chrome
+      electrum
+      exa
+      figlet
+      macchina
+      cloudflare-warp
+      inputs.alejandra.defaultPackage.${system}
+      # lxappearance
+      # glib
+
+      # image editor
+      # rawtherapee
+      # darktable
+      # gimp
+    ]
+    ++ [
+      # nur.repos.aleksana.gtkcord4
+      # nur.repos.aleksana.go-musicfox
+    ]);
+}
